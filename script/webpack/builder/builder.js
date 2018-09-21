70eb3de9603d7c09502915118b82459e557a46df
Rabbit <yfhj1990@hotmail.com>
2018-09-21 15:28:19 +0800

    Add builder test

--- script/webpack/builder/builder.js
+++ script/webpack/builder/builder.js
@@ -4,9 +4,9 @@
  * @flow
  */
 
-import { set, isFunction, isPlainObject, camelCase, defaults } from 'lodash'
+import { set, noop, isFunction, isPlainObject, camelCase, defaults } from 'lodash'
 import Entry from './entry'
-import type { WebpackOptions } from './webpack-options-type'
+import type { WebpackOptions, Mode as WebpackMode } from './webpack-options-type'
 
 
 /// code
@@ -16,16 +16,16 @@ type Options = {
 }
 
 class Builder {
+  mode: WebpackMode
+
   constructor(webpackOptions: webpackOptions = {}) {
-
     this.webpackOptions = webpackOptions
-
+    this.mode = webpackOptions.mode || Builder.readEnv('NODE_ENV') || 'development'
     this.entry = new Entry(webpackOptions.entry)
-
 
     this.transform = this.transform.bind(this)
 
-    [
+    this.export(this.entry, [
       'setEntry',
       'deleteEntry',
       'clearEntry',
@@ -34,25 +34,51 @@ class Builder {
       'clearEntryPrepends',
       'addEntryPrepend',
       'deleteEntryPrepend'
-    ].forEach(name => {
-      this[name] = this.entry[name].bind(this.entry)
-      this[name + 'Dev'] = this.entry[name].bind(this.entry)
-      this[name + 'Prod'] = this.entry[name].bind(this.entry)
-    })
+    ])
   }
 
-  _export() {
+  static readEnv(name: string): ?string {
+    if(!process.env) return null
 
+    return process.env[name]
+  }
+
+  callWithMode(mode: string, fn: Function): Function {
+    if(mode !== this.mode) {
+      return noop
+    }
+
+    return fn
+  }
+
+  export(entity: any, fns: Array<string>) {
+    fns.forEach(name => {
+      const fn = entity[name].bind(entity)
+
+      this[name] = fn
+      this[name + 'Dev'] = this.callWithMode('development', fn)
+      this[name + 'Prod'] = this.callWithMode('production', fn)
+    })
+
+    return this
   }
 
   transform(): WebpackOptions {
+    set(this.webpackOptions, 'mode', this.mode)
     set(this.webpackOptions, 'entry', this.entry.transform())
+
+    if(!this.webpackOptions.mode ||
+       !Object.keys(this.webpackOptions.entry).length) {
+      throw new Error(
+        `The webpack options "mode" and "entry" was required`
+      )
+    }
 
     return this.webpackOptions
   }
 }
 
-function builder(options) {
+function builder(options): Builder {
   return new Builder(options)
 }
 
@@ -68,22 +94,41 @@ import assert from 'assert'
 
 describe('Class Builder', () => {
   it('Builder.constructor', () => {
-    assert.deepStrictEqual(
-      {
-        entry: {
-          main: ['foo']
-        }
-      },
+    assert(new Builder())
+    assert(new Builder().transform)
+  })
 
-      new Builder({
-        entry: 'foo'
-      }).transform()
+  it('Builder#readEnv', () => {
+    process.env.FOO = 'foo'
+
+    assert.deepStrictEqual(
+      'foo',
+      Builder.readEnv('FOO')
+    )
+  })
+
+  it('Builder.callWithMode', () => {
+    const ref = () => {}
+
+    assert.deepStrictEqual(
+      ref,
+      new Builder({ mode: 'development' }).callWithMode('development', ref)
+    )
+  })
+
+  it('Builder.callWithMode not match Builder.mode', () => {
+    const ref = () => {}
+
+    assert.deepStrictEqual(
+      noop,
+      new Builder().callWithMode('production', ref)
     )
   })
 
   it('Builder.transform', () => {
     assert.deepStrictEqual(
       {
+        mode: 'test',
         entry: {
           main: ['foo']
         }
