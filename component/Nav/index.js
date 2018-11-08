f7412da7d354fe15cbb80003e54cfd862eecd5d9
Rabbit <yfhj1990@hotmail.com>
2018-11-08 17:27:19 +0800

    Add <Nav />

---
+++ component/Nav/index.js
@@ -0,0 +1,37 @@
+/**
+ * <Nav />
+ *
+ * @flow
+ */
+
+import * as React from 'react'
+import { classnames as cs } from '../../util'
+import reset from '../../style/reset.css'
+import style from './style.css'
+
+/// code
+
+export type Props = {
+  children?: React.Node,
+  classNames?: {
+    list?: string,
+    item?: string
+  },
+  className?: string,
+  tags?: {
+    list?: string,
+    item?: string
+  }
+}
+
+export default function Nav({ children, classNames = {}, className }: Props = {}): React.Node {
+  return (
+    <ul className={cs(reset.list, style.main, classNames.list, className)}>
+      {React.Children.map(children, (child, idx) => (
+        <li className={cs(style.item, className.item)} key={idx}>
+          {child}
+        </li>
+      ))}
+    </ul>
+  )
+}
