70dbdb80b6ebcf8165159e3da480bb886cb52f24
Rabbit <yfhj1990@hotmail.com>
2018-11-29 19:46:40 +0800

    Add babel plugin, transform computed value to React.useMemo

---
+++ script/babel/transform-react-usememo.js
@@ -0,0 +1,17 @@
+/**
+ * transform computed value to React.useMemo
+ *
+ * @flow
+ */
+
+export type Options = {
+
+}
+
+export default function transformReactUseMemo({ types: t }: any, { }: Options = {}) {
+  return {
+    visitor: {
+
+    }
+  }
+}
